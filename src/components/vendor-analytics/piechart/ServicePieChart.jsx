b747874c3bf516059ef9ab9b862fb1c23c90f32f
Bhagwan Biruly <b.biruly@Bhagwans-MacBook-Air.local>
2025-01-22 10:50:39 +0530

    service pie chart

--- src/components/vendor-analytics/piechart/ServicePieChart.jsx
+++ src/components/vendor-analytics/piechart/ServicePieChart.jsx
@@ -1,9 +1,11 @@
 import { PieChart, Pie, Tooltip, ResponsiveContainer } from "recharts";
-const ServicePieChart = (data) => {
+const ServicePieChart = ({data}) => {
 
-  if(!data.data) return null
-  const data02 = data?.data.map((item) => {
-    return {name: item.name, value: item.totalViews}
+  console.log(data)
+
+  if(!data) return null
+  const data02 = data?.map((item) => {
+    return {name: item?.name, value: item.totalViews}
   })  
 
   
