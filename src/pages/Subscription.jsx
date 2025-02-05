d26c602f74cbd429c3f7d96a70def489ad6b3c25
adb-santosh <santosh.atithidevobhava@gmail.com>
2025-02-05 17:10:36 +0530

    pay bug fixed

--- src/pages/Subscription.jsx
+++ src/pages/Subscription.jsx
@@ -56,7 +56,7 @@ function Subscription() {
 
             // Validate payment using fetch
             const validateResponse = await fetch(
-              `${VITE_API_URL}/api/v1/subscribe/verify-payment`, 
+              `http://localhost:4000/api/v1/subscribe/verify-payment`, 
               {
                 method: "POST",
                 headers: {
