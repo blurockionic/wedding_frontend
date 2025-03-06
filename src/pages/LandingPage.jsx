6529e66c274ac789ad9b1ef1a336b5726b5ef066
adb-santosh <santosh.atithidevobhava@gmail.com>
2025-03-06 16:08:35 +0530

    landing page layout oriantation changed

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,23 +1,19 @@
 import Home from "../pages/section/Home";
-
 import LazySection from "../components/LazySection";
 import Footer from "./Footer";
-import TopServiceSliderSection from "./HomeServiceSliderSection";
 import Testimonials from "../components/Testimonial";
 import Benefits from "./section/Benefit";
+import ServiceCategoriesPage from "./service-category/ServiceCategoriesPage";
+import Discover from "../components/home/home-discover/Discover";
+
 export default function LandingPage() {
   return (
-    <div >
+    <div>
       <LazySection id="/" Component={Home} />
-      {/* <LazySection id="about" Component={About} /> */}
-      {/* <LazySection id="gallery" Component={WeddingEvent} /> */}
-      <LazySection
-        id="topServiceSliderSection"
-        Component={TopServiceSliderSection}
-      />
+      <LazySection id="serviceCategories" Component={ServiceCategoriesPage} />
+      <LazySection id="discover" Component={Discover} />
       <LazySection id="benefit" Component={Benefits} />
       <LazySection id="testimonialSection" Component={Testimonials} />
-
       <LazySection id="footer" Component={Footer} />
     </div>
   );
