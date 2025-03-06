033f283a3254d648e3f045df0d0bdc0a6d5069e7
adb-santosh <santosh.atithidevobhava@gmail.com>
2025-03-06 15:51:08 +0530

    testimonial added

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,21 +1,24 @@
 import Home from "../pages/section/Home";
 
-
 import LazySection from "../components/LazySection";
 import Footer from "./Footer";
 import TopServiceSliderSection from "./HomeServiceSliderSection";
-
+import Testimonials from "../components/Testimonial";
+import Benefits from "./section/Benefit";
 export default function LandingPage() {
   return (
-    <div>
+    <div >
       <LazySection id="/" Component={Home} />
       {/* <LazySection id="about" Component={About} /> */}
       {/* <LazySection id="gallery" Component={WeddingEvent} /> */}
-      <LazySection id="topServiceSliderSection" Component={TopServiceSliderSection} />
-      {/* <LazySection id="contact" Component={Contact} /> */}
-     
+      <LazySection
+        id="topServiceSliderSection"
+        Component={TopServiceSliderSection}
+      />
+      <LazySection id="benefit" Component={Benefits} />
+      <LazySection id="testimonialSection" Component={Testimonials} />
+
       <LazySection id="footer" Component={Footer} />
-     
     </div>
   );
 }
