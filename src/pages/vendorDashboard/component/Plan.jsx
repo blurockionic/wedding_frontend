361f3508098ed3d688ac30ae188a9322547fcb9b
Bhagwan Biruly <b.biruly@Bhagwans-MacBook-Air.local>
2025-03-07 16:05:56 +0530

    plan card design enhanced

--- src/pages/vendorDashboard/component/Plan.jsx
+++ src/pages/vendorDashboard/component/Plan.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useGetPlansQuery } from "../../../redux/payment";
 import { formatPrice } from "../../../static/helper";
+import checkSvg from "../../../../public/plan/Check.svg"
 
 export default function Plan({ displayRazorpay }) {
   const { data, isLoading, isError, error } = useGetPlansQuery();
@@ -30,30 +31,31 @@ export default function Plan({ displayRazorpay }) {
           <div className="flex justify-center items-center">
             <div
               key={plan.id}
-              className="bg-card p-6 max-w-xs  h-fit py-10 rounded-lg shadow-custom shadow-pink-400 mb-6"
+              className="bg-card p-6 max-w-xs  h-fit py-10 rounded-lg border border-primary mb-6"
             >
               <h3 className="text-2xl text-center font-bold text-primary-foreground mb-4">
                 {plan.name}
               </h3>
-              <p className="text-muted-foreground   mb-4">{plan.description}</p>
-              <div className="flex justify-between items-center mb-4">
-                <span className="text-lg font-semibold text-muted-foreground">
+              {/* <p className="text-muted-foreground   mb-4">{plan.description}</p> */}
+              <div className="flex justify-center items-center mb-4">
+                <span className="text-xl font-bold text-primary">
                   {`${formatPrice(plan.price)} / ${plan.duration}`}
                 </span>
                 {/* <span className="text-muted-foreground text-sm">
                   Trial: {plan.trial_period} days
                 </span> */}
               </div>
+              <hr className="border-primary border-t-1 mt-5" />
 
               <div className="mb-4">
-                <h4 className="text-lg font-medium text-primary-foreground">
-                  Features
+                <h4 className="text-lg pt-5 text-primary-foreground font-thin">
+                  Includes:
                 </h4>
 
                 {plan.features && Object.keys(plan.features).length > 0 && (
-                  <ul className="list-disc pl-6 text-muted-foreground">
+                  <ul className="text-muted-foreground flex flex-col gap-3 mt-5">
                     {Object.entries(plan.features).map(([key, value]) => (
-                      <li key={key}>{`${
+                      <li key={key} className="flex gap-2 text-sm"><img src={checkSvg} alt="check"/>{`${
                         key.charAt(0).toUpperCase() + key.slice(1)
                       }: ${value}`}</li>
                     ))}
@@ -63,9 +65,9 @@ export default function Plan({ displayRazorpay }) {
 
               <button
                 onClick={() => displayRazorpay(plan.id)}
-                className="w-full hover:bg-primary text-primary-foreground py-2 rounded-lg mt-4 bg-accent focus:outline-none focus:ring-2 focus:ring-primary"
+                className="w-full bg-primary  text-background py-2 rounded-lg mt-4  focus:outline-none focus:ring-2 focus:ring-primary"
               >
-                Subscribe Now
+                Get Started
               </button>
             </div>
           </div>
