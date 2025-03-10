b08d90840603d10508cfe7144022a062c4e24727
adb-santosh <santosh.atithidevobhava@gmail.com>
2025-03-10 13:16:11 +0530

    expolre replced with slider

--- src/components/serviceList/ServiceList.jsx
+++ src/components/serviceList/ServiceList.jsx
@@ -2,32 +2,13 @@ import React from "react";
 import ServiceCard from "../ServiceCard";
 
 const ServiceList = ({ services }) => {
-  
-
-  // return (
-  //   <div className="relative  rounded-lg p-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-  //     {services.map((service, index) => (
-  //       <div key={service.id} className="relative">
-  //         <ServiceCard service={service} />
-  //         {index === services.length - 1 && (
-  //           <button className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white font-semibold rounded-lg">
-  //             <span className="bg-primary px-5 py-2 rounded-md">See More</span>
-  //           </button>
-  //         )}
-  //       </div>
-  //     ))}
-  //   </div>
-  // );
-
-return (
-  <div className="rounded-lg grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 gap-6">
-  {services.map((service, index) => (
-    <ServiceCard key={index} service={service} />
-  ))}
-</div>
-
-)
-
+  return (
+    <div className="flex  cursor-pointer gap-6">
+      {services.map((service, index) => (
+        <ServiceCard key={index} service={service} />
+      ))}
+    </div>
+  );
 };
 
 export default ServiceList;
