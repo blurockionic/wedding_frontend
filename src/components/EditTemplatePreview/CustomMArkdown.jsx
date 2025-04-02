ac4cdf7a8a2211788c57eef75e97637c6910c7fb
adb-santosh <santosh.atithidevobhava@gmail.com>
2025-04-02 12:03:15 +0530

    style(ui):improved appearance of service page

--- src/components/EditTemplatePreview/CustomMArkdown.jsx
+++ src/components/EditTemplatePreview/CustomMArkdown.jsx
@@ -1,58 +1,45 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 
-export default function CustomMArkdown({content}) {
+export default function CustomMarkdown({ content }) {
   return (
     <ReactMarkdown
       components={{
         h1: ({ node, ...props }) => (
-          <h1 className="text-3xl font-bold text-red-600 mb-4" {...props} />
+          <h1 className="font-montserrat text-2xl font-bold text-red-600 mb-3 leading-tight" {...props} />
         ),
         h2: ({ node, ...props }) => (
-          <h2
-            className="text-2xl capitalize font-semibold text-gray-800 mt-6 mb-3"
-            {...props}
-          />
+          <h2 className="font-montserrat text-2xl capitalize font-semibold text-gray-800 mt-5 mb-2 leading-tight" {...props} />
         ),
         h3: ({ node, ...props }) => (
-          <h3
-            className="text-xl  capitalize font-medium text-gray-700 mt-4 mb-2"
-            {...props}
-          />
+          <h3 className="font-montserrat text-xl capitalize font-medium text-gray-700 mt-3 mb-2 leading-snug" {...props} />
         ),
         p: ({ node, ...props }) => (
-          <p
-            className="text-lg text-gray-600 leading-relaxed mt-2 mb-4"
-            {...props}
-          />
+          <p className="font-montserrat text-lg text-gray-600 leading-snug mt-2 mb-3" {...props} />
         ),
         ul: ({ node, ...props }) => (
-          <ul
-            className="list-disc ml-6 text-gray-700 mt-2 mb-4 space-y-2"
-            {...props}
-          />
+          <ul className="font-montserrat ml-6 text-gray-700 mt-2 mb-3 space-y-1 leading-snug" {...props} />
         ),
         ol: ({ node, ...props }) => (
-          <ol
-            className="list-decimal ml-6 text-gray-700 mt-2 mb-4 space-y-2"
-            {...props}
-          />
+          <ol className="font-montserrat list-decimal ml-6 text-gray-700 mt-2 mb-3 space-y-1 leading-snug" {...props} />
         ),
         li: ({ node, ...props }) => (
-          <li className="text-lg leading-snug" {...props} />
+          <li className="font-montserrat  relative text-lg leading-snug flex items-start float-left gap-2">
+            <span className="bg-green-500  mt-1.5  text-white font-semibold text-sm px-1 rounded-md">
+              ✔
+            </span>
+            <span className="flex-1  " {...props} />
+          </li>
         ),
         strong: ({ node, ...props }) => (
-          <strong className="font-semibold text-gray-900" {...props} />
+          <strong className="font-montserrat font-semibold text-gray-900" {...props} />
         ),
         blockquote: ({ node, ...props }) => (
-          <blockquote
-            className="border-l-4 border-gray-400 pl-4 italic text-gray-700 my-4"
-            {...props}
-          />
+          <blockquote className="font-montserrat border-l-4 border-gray-400 pl-4 italic text-gray-700 my-3 leading-snug" {...props} />
         ),
       }}
     >
-     {content}
+      {content}
     </ReactMarkdown>
   );
 }
