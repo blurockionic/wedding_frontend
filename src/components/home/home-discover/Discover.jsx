1b18495811a0a2be143cf69e74cd53eeed3dd4f7
Bhagwan Biruly <b.biruly@Bhagwans-MacBook-Air.local>
2025-04-10 11:03:02 +0530

    [update] add top bridal service section in home page

--- src/components/home/home-discover/Discover.jsx
+++ src/components/home/home-discover/Discover.jsx
@@ -51,6 +51,19 @@ const Discover = () => {
       },
     },
     {
+      key: "topBridal",
+      title: "Top Bridal serives",
+      subtitle: "Find top bridal services in your city",
+      bgColor: "#ddd6fe",
+      filter: {
+        limit: 20,
+        page: 1,
+        service_type: "Makeup Salon",
+        sort_by: "rating",
+        sort_order: "desc",
+      },
+    },
+    {
       key: "topDj",
       title: "Top DJ Services",
       subtitle: "Find top DJ services in your city",
